e19391cb80f05a1e4d75631091b83fa090a60487
emlog <emlog@qq.com>
2020-10-07 09:36:31 +0800

    update

---
+++ go-expression/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+//var a = "hello world"
+
+func main() {
+	fmt.Println("hello world!!")
+
+	//定义切片
+	s := []int{1, 2, 3}
+
+	//遍历切片
+	for _, value := range s {
+		fmt.Printf("%d , %s ", value, "---")
+	}
+
+}
