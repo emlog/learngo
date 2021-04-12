974229c6b4408e6da110caf6e0e62f7294dca87b
xudawei <xudawei@luojilab.com>
2021-04-12 19:10:35 +0800

    update

---
+++ example/string_test/concat_string_test.go
@@ -0,0 +1,64 @@
+// 字符串连接性能测试。
+package string_test
+
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+const numbers = 100
+
+// fmt.Sprintf 连接方式
+func BenchmarkSprintf(b *testing.B) {
+	b.ResetTimer()
+	for idx := 0; idx < b.N; idx++ {
+		var s string
+		for i := 0; i < numbers; i++ {
+			s = fmt.Sprintf("%v%v", s, i)
+		}
+	}
+	b.StopTimer()
+}
+
+// builder 连接方式（效率最高 推荐）
+func BenchmarkStringBuilder(b *testing.B) {
+	b.ResetTimer()
+	for idx := 0; idx < b.N; idx++ {
+		var builder strings.Builder
+		for i := 0; i < numbers; i++ {
+			builder.WriteString(strconv.Itoa(i))
+
+		}
+		_ = builder.String()
+	}
+	b.StopTimer()
+}
+
+// buf 连接方式
+func BenchmarkBytesBuf(b *testing.B) {
+	b.ResetTimer()
+	for idx := 0; idx < b.N; idx++ {
+		var buf bytes.Buffer
+		for i := 0; i < numbers; i++ {
+			buf.WriteString(strconv.Itoa(i))
+		}
+		_ = buf.String()
+	}
+	b.StopTimer()
+}
+
+// StringAdd 方式
+func BenchmarkStringAdd(b *testing.B) {
+	b.ResetTimer()
+	for idx := 0; idx < b.N; idx++ {
+		var s string
+		for i := 0; i < numbers; i++ {
+			s += strconv.Itoa(i)
+		}
+
+	}
+	b.StopTimer()
+}
