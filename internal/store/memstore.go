1eaec00031f0b1690813fe23dae044844a18e687
xudawei <xudawei@luojilab.com>
2021-11-01 17:46:38 +0800

    update

--- internal/store/memstore.go
+++ internal/store/memstore.go
@@ -1,1 +1,28 @@
 package store
+
+import (
+	"sync"
+
+	"github.com/emlog/goexample/store/factory"
+)
+
+// internal/store/memstore.go
+
+package store
+
+import (
+mystore "bookstore/store"
+factory "bookstore/store/factory"
+"sync"
+)
+
+func init() {
+	factory.Register("mem", &MemStore{
+		books: make(map[string]*mystore.Book),
+	})
+}
+
+type MemStore struct {
+	sync.RWMutex
+	books map[string]*mystore.Book
+}
--- server/server.go
+++ server/server.go
@@ -1,1 +1,78 @@
 package server
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/emlog/goexample/store"
+)
+
+// server/server.go
+
+type BookStoreServer struct {
+	s   store.Store
+	srv *http.Server
+}
+
+// server/server.go
+
+func NewBookStoreServer(addr string, s store.Store) *BookStoreServer {
+	srv := &BookStoreServer{
+		s: s,
+		srv: &http.Server{
+			Addr: addr,
+		},
+	}
+
+	router := mux.NewRouter()
+	router.HandleFunc("/book", srv.createBookHandler).Methods("POST")
+	router.HandleFunc("/book/{id}", srv.updateBookHandler).Methods("POST")
+	router.HandleFunc("/book/{id}", srv.getBookHandler).Methods("GET")
+	router.HandleFunc("/book", srv.getAllBooksHandler).Methods("GET")
+	router.HandleFunc("/book/{id}", srv.delBookHandler).Methods("DELETE")
+
+	srv.srv.Handler = middleware.Logging(middleware.Validating(router))
+	return srv
+}
+
+// server/server.go
+
+func (bs *BookStoreServer) createBookHandler(w http.ResponseWriter, req *http.Request) {
+	dec := json.NewDecoder(req.Body)
+	var book store.Book
+	if err := dec.Decode(&book); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err := bs.s.Create(&book); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+}
+
+func (bs *BookStoreServer) getBookHandler(w http.ResponseWriter, req *http.Request) {
+	id, ok := mux.Vars(req)["id"]
+	if !ok {
+		http.Error(w, "no id found in request", http.StatusBadRequest)
+		return
+	}
+
+	book, err := bs.s.Get(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	response(w, book)
+}
+
+func response(w http.ResponseWriter, v interface{}) {
+	data, err := json.Marshal(v)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(data)
+}
