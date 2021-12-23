78ac244915407a2d166d22eebb8ea36c783167d3
xudawei <xudawei@luojilab.com>
2021-12-23 18:53:22 +0800

    update

--- dao/note/note.go
+++ dao/note/note.go
@@ -1,40 +1,38 @@
 package note
 
 import (
-	"database/sql"
+	"log"
 
 	"github.com/emlog/goexample/model/request"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-xorm/xorm"
 )
 
 type NoteDao struct {
-	*sql.DB // 数据库连接
+	*xorm.Engine // 数据库连接
 }
 
 func NewNoteDao() *NoteDao {
 	// DSN (Data Source Name)
 	// [username[:password]@][protocol[(address)]]/dbname[?param1=value1&...&paramN=valueN]
-	db, err := sql.Open("mysql", "root:12345678@(127.0.0.1)/test_note")
+	engine, err := xorm.NewEngine("mysql", "root:12345678@(127.0.0.1)/test_note?charset=utf8")
+
 	if err != nil {
-		panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
+		log.Fatal(err)
 	}
 
-	return &NoteDao{db}
+	return &NoteDao{engine}
 }
 
 // Create 创建
-func (d NoteDao) Create(req *request.ReqNote) (id int64, err error) {
+func (d NoteDao) Create(note *request.ReqNote) (id int64, err error) {
+
 	// 执行插入操作
-	result, err := d.Exec("INSERT INTO note(content) VALUES(?)", req.Content)
+	affected, err := d.Table("note").Insert(note)
 	if err != nil {
-		return
+		return 0, err
 	}
 
-	// 获取插入数据的主键
-	id, err = result.LastInsertId()
-	if err != nil {
-		return
-	}
-
-	return id, nil
+	// 返回影响行数
+	return affected, nil
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751
 	github.com/gin-gonic/gin v1.7.7
 	github.com/go-sql-driver/mysql v1.6.0
+	github.com/go-xorm/xorm v0.7.9
 	github.com/stretchr/testify v1.7.0
 	github.com/swaggo/swag v1.7.4
 )
