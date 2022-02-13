03d690a5b11473c95c2f95deb0e45af4d0a7b2d6
snow <emloog@gmail.com>
2022-02-13 20:34:06 +0800

    update

--- main.go
+++ main.go
@@ -1,25 +1,42 @@
 package main
 
 import (
-	"flag"
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/emlog/goexample/api"
 )
 
-var (
-	intflag    int
-	boolflag   bool
-	stringflag string
-)
-
+// @title 简易笔记工具
+// @version 1.0
+// @description 简易笔记发布系统 - GO学习参考项目
+// @termsOfService https://github.com/emlog/goexample
 func main() {
 
-	flag.IntVar(&intflag, "intflag", 0, "int flag value")
-	flag.BoolVar(&boolflag, "boolflag", false, "bool flag value")
-	flag.StringVar(&stringflag, "stringflag", "default", "string flag value")
+	// 创建监听退出chan
+	c := make(chan os.Signal)
+	// 监听指定信号 ctrl+c kill
+	signal.Notify(c, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGUSR1, syscall.SIGUSR2)
+	go func() {
+		for s := range c {
+			switch s {
+			case syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT:
+				fmt.Println("退出", s)
+			case syscall.SIGUSR1:
+				fmt.Println("usr1", s)
+			case syscall.SIGUSR2:
+				fmt.Println("usr2", s)
+			default:
+				fmt.Println("other", s)
+			}
+		}
+	}()
 
-	flag.Parse()
-
-	fmt.Println("int flag:", intflag)
-	fmt.Println("bool flag:", boolflag)
-	fmt.Println("string flag:", stringflag)
+	router := gin.Default()
+	api.Router(router)
+	router.Run(":5956")
 }
