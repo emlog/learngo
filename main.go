b3f787e9b61dfd9b9b5b5f001e18b4d2fc753e1e
xudawei <xudawei@luojilab.com>
2022-02-16 18:25:13 +0800

    update

--- main.go
+++ main.go
@@ -1,25 +1,13 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-)
-
-var (
-	intflag    int
-	boolflag   bool
-	stringflag string
+	"github.com/emlog/goexample/api"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
-	flag.IntVar(&intflag, "intflag", 0, "int flag value")
-	flag.BoolVar(&boolflag, "boolflag", false, "bool flag value")
-	flag.StringVar(&stringflag, "stringflag", "default", "string flag value")
-
-	flag.Parse()
-
-	fmt.Println("int flag:", intflag)
-	fmt.Println("bool flag:", boolflag)
-	fmt.Println("string flag:", stringflag)
+	router := gin.Default()
+	api.Router(router)
+	router.Run(":5956")
 }
