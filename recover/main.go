bff3ce564d1bbef0c2d99932266420bf2d144f19
xudawei <xudawei@luojilab.com>
2022-04-07 17:10:47 +0800

    update

---
+++ recover/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := []string{"a", "b"}
+	go checkAndPrintWithRecover(a, 2)
+	fmt.Println("Exiting normally")
+}
+
+func checkAndPrintWithRecover(a []string, index int) {
+	defer handleOutOfBounds()
+	checkAndPrint(a, 2)
+}
+
+func checkAndPrint(a []string, index int) {
+	if index > (len(a) - 1) {
+		panic("Out of bound access for slice")
+	}
+	fmt.Println(a[index])
+}
+
+// recover function in the same goroutine as panic
+func handleOutOfBounds() {
+	if r := recover(); r != nil {
+		fmt.Println("Recovering from panic:", r)
+	}
+}
